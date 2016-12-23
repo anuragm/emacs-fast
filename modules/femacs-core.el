6306c7e38579f58b4901bd395eda0b4c265783d9
Anurag Mishra <anuragmi@usc.edu>
2016-12-22 16:39:47 -0800

    Edit files as super user

--- modules/femacs-core.el
+++ modules/femacs-core.el
@@ -76,6 +76,20 @@
                  (lambda (arg1 arg2)
                    (string< (car arg1) (car arg2))))))))
 
+;; Edit root files as super user.
+;; Taken from http://emacsredux.com/blog/2013/04/21/edit-files-as-root/
+(defun sudo-edit (&optional arg)
+  "Edit currently visited file as root.
+
+With a prefix ARG prompt for a file to visit.
+Will also prompt for a file to visit if current
+buffer is not visiting a file."
+  (interactive "P")
+  (if (or arg (not buffer-file-name))
+      (find-file (concat "/sudo:root@localhost:"
+                         (ido-read-file-name "Find file(as root): ")))
+    (find-alternate-file (concat "/sudo:root@localhost:" buffer-file-name))))
+
 ;;Kill everything except current buffer, *scratch* and *Messages*
 (defun kill-rest-buffers ()
   "Kill all the other buffers except this buffer, *scratch* and *Messages*."
