0ce934452f3bf19f7e3c1703f202ffc90ff77b2a
Anurag Mishra <anuragmi@usc.edu>
2019-01-08 11:17:22 -0800

    Benchmark load times

--- init.el
+++ init.el
@@ -85,6 +85,13 @@
   :ensure t
   :commands (diminish))
 
+;; Add benchmarking code
+(use-package benchmark-init
+  :ensure t
+  :config
+  ;; To disable collection of benchmark data after init is done.
+  (add-hook 'after-init-hook 'benchmark-init/deactivate))
+
 ;; Bootstrap QELPA.
 (defvar quelpa-checkout-melpa-p)
 (defvar quelpa-melpa-recipe-stores)
