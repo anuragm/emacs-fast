738862a5df7a3ca358bd1be6c2b602ebbb9eb066
Anurag Mishra <anurag@mishra.xyz>
2021-07-16 14:06:38 -0700

    Add JSON mode

---
+++ modules/lang/femacs-json.el
@@ -0,0 +1,49 @@
+;;; femacs-json.el --- Module for editing JSON files
+;;
+;; Copyright © 2016-2021 Anurag Mishra
+;;
+;; Author: Anurag Mishra
+;; URL: https://github.com/anuragm/emacs-fast
+;; Version: 1.0.0
+;; Keywords: convenience
+
+;; This file is not part of GNU Emacs.
+
+;;; Commentary:
+
+;; Contains packages to edit, check and format JSON files.
+
+;;; License:
+
+;; Copyright (c) 2016-2021 Anurag Mishra
+
+;; Permission is hereby granted, free of charge, to any person obtaining a copy
+;; of this software and associated documentation files (the "Software"), to deal
+;; in the Software without restriction, including without limitation the rights
+;; to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+;; copies of the Software, and to permit persons to whom the Software is
+;; furnished to do so, subject to the following conditions:
+;;
+;; The above copyright notice and this permission notice shall be included in all
+;; copies or substantial portions of the Software.
+;;
+;; THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+;; IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+;; FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+;; AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+;; LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+;; OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+;; SOFTWARE.
+
+;;; Code:
+
+(use-package json-mode
+  :ensure t)
+
+(use-package json-reformat
+  :ensure t
+  :custom
+  (json-reformat:indent-width 2))
+
+(provide 'femacs-json)
+;;; femacs-json.el ends here
