cf5a179343e0a58d94f40f2a7c0c488e5dd2f8e0
Anurag Mishra <agmishra@amazon.com>
2026-01-24 02:14:38 -0800

    Remove custom flycheck ruff
    It has been included in upstream flycheck, and we also use ruff LSP server. So
    this is no longer needed.

--- modules/femacs-check.el
+++ modules/femacs-check.el
@@ -88,7 +88,6 @@
   (flycheck-emacs-lisp-load-path 'inherit)   ; Let flycheck.el see loaded paths.
   :defer t
   :config
-  (require 'flycheck-ruff)
   (diminish 'flycheck-mode "Ⓕ"))
 
 (provide 'femacs-check)
