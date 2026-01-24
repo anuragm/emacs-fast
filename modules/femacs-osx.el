0875796470964e614256b33eaaf3b2ad6463b537
Anurag Mishra <agmishra@amazon.com>
2026-01-24 01:52:14 -0800

    More intelligent path loading

--- modules/femacs-osx.el
+++ modules/femacs-osx.el
@@ -56,26 +56,36 @@
   (featurep 'mac))
 
 
-;; Read path variable from command line, if not manually customized.
+;; Read path variable from command line, with caching for fast startup.
 (defcustom emacs-fast/exec-path nil
-  "Custom exec path for Emacs.
+  "Cached exec path for Emacs.
 
-When not called from shell, Emacs does not inherits $PATH from shell.  When nil,
+When not called from shell, Emacs does not inherit $PATH from shell.  When nil,
 paths are read by launching a login shell, which might be slow with complicated
-configuration.  Manually specifying the path will make the process faster."
+configuration.  This variable is automatically populated and kept in sync."
   :group 'emacs-fast
   :type '(repeat directory))
+
+(use-package exec-path-from-shell
+  :commands (exec-path-from-shell-initialize exec-path-from-shell-getenvs))
+
+(defun emacs-fast/sync-exec-path ()
+  "Sync `'exec-path`' from shell if it differs from cached."
+  (require 'exec-path-from-shell)
+  (let ((shell-path (car (exec-path-from-shell-getenvs '("PATH")))))
+    (unless (equal (getenv "PATH") (cdr shell-path))
+      (exec-path-from-shell-initialize)
+      (customize-save-variable 'emacs-fast/exec-path exec-path)
+      (message "Synced exec-path from shell"))))
 
 (if emacs-fast/exec-path
     (progn
       (setq exec-path emacs-fast/exec-path)
-      (setenv "PATH" (mapconcat 'identity exec-path ":")))
-  (use-package exec-path-from-shell
-    :demand
-    :functions exec-path-from-shell-initialize
-    :config
-    (when (memq window-system '(mac ns))
-      (exec-path-from-shell-initialize))))
+      (setenv "PATH" (mapconcat 'identity exec-path ":"))
+      (run-with-idle-timer 60 nil #'emacs-fast/sync-exec-path))
+  (when (memq window-system '(mac ns))
+    (exec-path-from-shell-initialize)
+    (customize-save-variable 'emacs-fast/exec-path exec-path)))
 
 
 ;; Don't delete directly, use Trash. Inform if 'trash' utility has not been installed.
